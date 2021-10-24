172e0d3ccc3ea0f6ff4608a4de4c1c4d8b5557c0
TheRowan <rowanaylett@rocketmail.com>
2021-10-25 01:30:40 +1100

    Added UI Element

--- code/ui/BatteryLevel.cs
+++ code/ui/BatteryLevel.cs
@@ -5,24 +5,24 @@ using Sandbox.UI.Tests;
 
 namespace scarebox
 {
-    public class BatteryLevel : Panel
-    {
+	public class BatteryLevel : Panel
+	{
 		public Label batteryLabel;
 
-        public BatteryLevel()
+		public BatteryLevel()
 		{
-			batteryLabel = Add.Label("100", "batterylevel");
+			batteryLabel = Add.Label( "100", "batterylevel" );
 		}
 
 		public override void Tick()
 		{
 			var player = Local.Pawn;
-			if (player == null) return;
+			if ( player == null ) return;
 
-			Entity ent = Entity.FindByName("Flashlight");
-			if (!player.Inventory.Contains(ent)) return;
+			Entity ent = Entity.FindByName( "Flashlight" ); // DO THIS BETTER
+			if ( !player.Inventory.Contains( ent ) ) return;
 
 			batteryLabel.Text = $"{ent.Name}";
 		}
-    }
+	}
 }
